bb662f9ceb1f2c19112e352a1e55c95e96f73dd3
timothy tran <tadeothien@gmail.com>
2022-04-23 09:41:35 +0700

    update Bookmark

--- lib/Screen/bookmark.dart
+++ lib/Screen/bookmark.dart
@@ -129,6 +129,116 @@ Widget getBookmark() {
                 );
               }))),
         ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 30, right: 30, bottom: 10, top: 30),
+              child: Text("Fast foods",
+                  style: TextStyle(
+                      color: UI.appColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500)),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                  padding: EdgeInsets.only(left: 5),
+                  child: Row(
+                      children: List.generate(10, (index) {
+                    return Padding(
+                      padding: EdgeInsets.only(left: 30),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 250,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(songs[index]['img']),
+                                      fit: BoxFit.cover),
+                                  color: UI.appColor),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              songs[index]['title'],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }))),
+            ),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 30, right: 30, bottom: 10, top: 30),
+              child: Text("Drinks",
+                  style: TextStyle(
+                      color: UI.appColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500)),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                  padding: EdgeInsets.only(left: 5),
+                  child: Row(
+                      children: List.generate(10, (index) {
+                    return Padding(
+                      padding: EdgeInsets.only(left: 30),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 250,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(drinks[index]['img']),
+                                      fit: BoxFit.cover),
+                                  color: UI.appColor),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              drinks[index]['title'],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }))),
+            ),
+          ],
+        ),
       ],
     ),
   ]));
