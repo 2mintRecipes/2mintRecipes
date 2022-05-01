b2a67f793f3aeaa6d87a4e313ca314d288a4ce4d
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-05-01 10:58:35 +0700

    update `Profile` screen

--- lib/Screen/profile/profile.dart
+++ lib/Screen/profile/profile.dart
@@ -24,131 +24,209 @@ class _ProfileState extends State<Profile> {
   }
 
   Widget getBody() {
-    return Padding(
-        padding:
-            const EdgeInsets.only(top: 15, bottom: 15, left: 30, right: 30),
-        child: SingleChildScrollView(
-          child: Column(
+    return SingleChildScrollView(
+      controller: ScrollController(),
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text(
-                "My Profile",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
+              const GFAvatar(
+                backgroundImage: AssetImage("assets/images/nong-nao-doll.jpg"),
+                shape: GFAvatarShape.circle,
+                size: 100,
               ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                // crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const GFAvatar(
-                    backgroundImage:
-                        AssetImage("assets/images/nong-nao-doll.jpg"),
-                    shape: GFAvatarShape.circle,
-                    size: 100,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Nguyễn Thanh Tiến",
-                          style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          "@TienNHM",
-                          style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                              fontStyle: FontStyle.italic),
-                        ),
-                        const SizedBox(height: 10),
-                        OutlinedButton(
-                          onPressed: () {},
-                          child: const Text('Chỉnh sửa'),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 10),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Trending Foods For You",
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Nguyễn Thanh Tiến",
                       style: TextStyle(
-                          color: UI.appColor,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500)),
-                  const SizedBox(height: 10),
-                  GridView.count(
-                      // Create a grid with 2 columns. If you change the scrollDirection to
-                      // horizontal, this produces 2 rows.
-                      crossAxisCount: 2,
-                      shrinkWrap: true,
-                      controller: ScrollController(),
-                      // Generate 100 widgets that display their index in the List.
-                      children: List.generate(10, (index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 30),
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Column(
-                              children: [
-                                Container(
-                                  width: 250,
-                                  height: 150,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image:
-                                              AssetImage(songs[index]['img']),
-                                          fit: BoxFit.cover),
-                                      color: UI.appColor),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  songs[index]['title'],
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                SizedBox(
-                                  width: 180,
-                                  child: Text(
-                                    songs[index]['description'],
-                                    maxLines: 1,
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.grey,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      }))
-                ],
-              ),
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "@TienNHM",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    const SizedBox(height: 10),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Chỉnh sửa'),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
-        ));
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Recipes",
+                      style: TextStyle(
+                        // fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "14",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                    height: 40,
+                    child: VerticalDivider(
+                      color: Colors.grey,
+                      thickness: 1.5,
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Followers",
+                      style: TextStyle(
+                        // fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Following",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                    height: 40,
+                    child: VerticalDivider(
+                      color: Colors.grey,
+                      thickness: 1.5,
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Recipes",
+                      style: TextStyle(
+                        // fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "14",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Text("My Gallary",
+                  style: TextStyle(
+                      color: UI.appColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500)),
+              const SizedBox(height: 20),
+              GridView.count(
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  crossAxisSpacing: 20,
+                  // padding: const EdgeInsets.all(10),
+                  controller: ScrollController(),
+                  children: List.generate(10, (index) {
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 250,
+                            height: 150,
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 2, color: UI.appColor),
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                    image: AssetImage(songs[index]['img']),
+                                    fit: BoxFit.cover),
+                                color: Colors.white),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            songs[index]['title'],
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          SizedBox(
+                            width: 180,
+                            child: Text(
+                              songs[index]['description'],
+                              maxLines: 1,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }))
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
