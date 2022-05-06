300fea34a26b8f9351dfecaa5687efafb92604c3
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-05-07 00:21:55 +0700

    update `Bookmark`

--- lib/Screen/bookmark.dart
+++ lib/Screen/bookmark.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_first/app_ui.dart';
+import 'package:flutter_first/components/search_cart.dart';
 
 import '../database.dart';
 
@@ -16,230 +19,164 @@ class _BookmarkState extends State<Bookmark> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: getBookmark(),
+      backgroundColor: Colors.transparent,
+      body: Stack(children: [
+        SafeArea(
+            child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  filterQuality: FilterQuality.low,
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                      Colors.white.withOpacity(1), BlendMode.darken),
+                  image: const AssetImage("images/bg.jpg"))),
+        )),
+        getBody(),
+      ]),
     );
   }
-}
 
-Widget getBookmark() {
-  return SingleChildScrollView(
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-    const Padding(
-        padding: const EdgeInsets.only(left: 30, top: 20),
-        child: Text(
-          "Your bookmark",
-          style: TextStyle(
-              fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
-        )),
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 30, top: 20),
-            child: Row(
-                children: List.generate(song_type_1.length, (index) {
-              return Padding(
-                padding: const EdgeInsets.only(right: 25),
-                child: GestureDetector(
-                  onTap: () {
-                    // setState(() {
-                    //   activeMenu = index;
-                    // });
-                  },
-                  child: Column(
+  Widget getBody() {
+    return ClipRRect(
+        // borderRadius: BorderRadius.circular(5),
+        child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 90, sigmaY: 90),
+            child: SingleChildScrollView(
+                child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(width: 2, color: UI.appColor),
-                              borderRadius: BorderRadius.circular(15),
-                              color: UI.appColor),
-                          child: Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Text(
-                              song_type_1[index],
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-              );
-            })),
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Padding(
-              padding: EdgeInsets.only(left: 5),
-              child: Row(
-                  children: List.generate(10, (index) {
-                return Padding(
-                  padding: EdgeInsets.only(left: 30),
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 250,
-                          height: 150,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage(songs[index]['img']),
-                                  fit: BoxFit.cover),
-                              color: UI.appColor),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          songs[index]['title'],
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Container(
-                          width: 180,
-                          child: Text(
-                            songs[index]['description'],
-                            maxLines: 1,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        )
-                      ],
+                  /// Title
+                  getTitle(),
+
+                  /// Search card
+                  Padding(
+                      padding:
+                          const EdgeInsets.only(left: 30, top: 20, right: 30),
+                      child: SearchCard()),
+
+                  /// Get Bookmark Type
+                  getBookmarkType(),
+
+                  /// Bookmark Items
+                  getListBookmarkItems(),
+                ]))));
+  }
+
+  Widget getTitle() {
+    return Padding(
+        padding:
+            const EdgeInsets.only(left: 30, right: 20, bottom: 10, top: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Bookmark",
+                style: TextStyle(
+                    color: Colors.white.withOpacity(.8),
+                    fontSize: 27,
+                    fontWeight: FontWeight.w600)),
+          ],
+        ));
+  }
+
+  Widget getBookmarkType() {
+    return SingleChildScrollView(
+      //lists with kind tags
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.only(left: 30, top: 20, bottom: 20),
+      child: Row(
+          children: List.generate(song_type_1.length, (index) {
+        ////////// list tags
+        return Padding(
+          padding: const EdgeInsets.only(right: 25),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                activeMenu = index;
+              });
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black.withOpacity(.1),
                     ),
+                    borderRadius: BorderRadius.circular(15),
+                    color: activeMenu == index
+                        ? UI.appColor
+                        : Colors.black.withOpacity(.1),
                   ),
-                );
-              }))),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding:
-                  EdgeInsets.only(left: 30, right: 30, bottom: 10, top: 30),
-              child: Text("Fast foods",
+                  padding: const EdgeInsets.only(
+                      top: 7, bottom: 7, left: 15, right: 15),
+                  child: Text(
+                    song_type_1[index],
+                    style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      })),
+    );
+  }
+
+  Widget getListBookmarkItems() {
+    return GridView.count(
+        crossAxisCount: 2,
+        shrinkWrap: true,
+        crossAxisSpacing: 20,
+        padding: const EdgeInsets.only(left: 30, right: 30, top: 20),
+        controller: ScrollController(),
+        children: List.generate(10, (index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Column(
+              children: [
+                Container(
+                  width: 250,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 2, color: Colors.white.withOpacity(.4)),
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                          image: AssetImage(songs[index]['img']),
+                          fit: BoxFit.cover),
+                      color: Colors.white.withOpacity(.4)),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  songs[index]['title'],
                   style: TextStyle(
                       color: UI.appColor,
                       fontSize: 20,
-                      fontWeight: FontWeight.w500)),
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                SizedBox(
+                  width: 180,
+                  child: Text(
+                    songs[index]['description'],
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white.withOpacity(.8),
+                        fontWeight: FontWeight.w600),
+                  ),
+                )
+              ],
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Padding(
-                  padding: EdgeInsets.only(left: 5),
-                  child: Row(
-                      children: List.generate(10, (index) {
-                    return Padding(
-                      padding: EdgeInsets.only(left: 30),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 250,
-                              height: 150,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage(songs[index]['img']),
-                                      fit: BoxFit.cover),
-                                  color: UI.appColor),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              songs[index]['title'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }))),
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding:
-                  EdgeInsets.only(left: 30, right: 30, bottom: 10, top: 30),
-              child: Text("Drinks",
-                  style: TextStyle(
-                      color: UI.appColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500)),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Padding(
-                  padding: EdgeInsets.only(left: 5),
-                  child: Row(
-                      children: List.generate(10, (index) {
-                    return Padding(
-                      padding: EdgeInsets.only(left: 30),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 250,
-                              height: 150,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage(drinks[index]['img']),
-                                      fit: BoxFit.cover),
-                                  color: UI.appColor),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              drinks[index]['title'],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }))),
-            ),
-          ],
-        ),
-      ],
-    ),
-  ]));
+          );
+        }));
+  }
 }
