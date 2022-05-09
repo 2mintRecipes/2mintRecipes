601b80e2c57fd70b9b04fbbf925f1b628506362c
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-05-09 22:07:54 +0700

    refactor code

--- lib/Screen/login.dart
+++ lib/Screen/login.dart
@@ -25,131 +25,136 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     // Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Stack(
-      children: [
-        SafeArea(
+      body: Stack(
+        children: [
+          SafeArea(
             child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              filterQuality: FilterQuality.low,
-              fit: BoxFit.fitHeight,
-              colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.6),
-                BlendMode.darken,
-              ),
-              image: AssetImage("assets/images/welcome_bg.jpg"),
-            ),
-          ),
-        )),
-        SingleChildScrollView(
-          controller: ScrollController(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.4,
-                child: Transform.scale(
-                  scale: 0.6,
-                  child: Image.asset(UI.appLogo),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  filterQuality: FilterQuality.low,
+                  fit: BoxFit.fitHeight,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.6),
+                    BlendMode.darken,
+                  ),
+                  image: AssetImage("assets/images/welcome_bg.jpg"),
                 ),
               ),
-              Container(
-                  padding: EdgeInsets.all(10),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        height: MediaQuery.of(context).size.height * 0.4,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  // Text(
-                                  //   "LOG IN",
-                                  //   style: TextStyle(
-                                  //       fontSize: 30, fontWeight: FontWeight.bold),
-                                  // ),
-                                  Input(
-                                    icon: Icons.alternate_email_outlined,
-                                    hintText: 'Email',
-                                    isPassword: false,
-                                    isEmail: true,
-                                    textController: emailController,
-                                  ),
-                                  Input(
-                                    icon: Icons.lock_outline_sharp,
-                                    hintText: 'Password',
-                                    isPassword: true,
-                                    isEmail: false,
-                                    textController: passwordController,
-                                  ),
-                                  // Password(hintText: "Password"),
-                                  TextButton(
-                                    onPressed: () {
-                                      loginWithUsernamePassword();
-                                    },
-                                    child: Text("Login"),
-                                    style: TextButton.styleFrom(
-                                      primary: Colors.white,
-                                      backgroundColor: UI.appColor,
-                                    ),
+            ),
+          ),
+          getBody(),
+        ],
+      ),
+    );
+  }
 
-                                    // title: 'Log In',
-                                    // buttonColor: UI.appColor,
-                                    // textColor: Colors.white,
-                                    // destination: '/root',
-                                    // icon: Icon(Icons.login_rounded),
-                                    // onTap: () {},
-                                  )
-                                ]),
-                            Container(
-                              padding: EdgeInsets.only(top: 30),
-                              width: 300,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pushNamed(context, '/signup');
-                                    },
-                                    child: Text(
-                                      'Register',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 17),
-                                    ),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pushNamed(context, '/forgot');
-                                    },
-                                    child: Text(
-                                      'Forgot password?',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 17),
-                                    ),
-                                  ),
-                                ],
+  Widget getBody() {
+    return SingleChildScrollView(
+      controller: ScrollController(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: Transform.scale(
+              scale: 0.6,
+              child: Image.asset(UI.appLogo),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "LOG IN",
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
                               ),
+                            ),
+                            Input(
+                              icon: Icons.alternate_email_outlined,
+                              hintText: 'Email',
+                              isPassword: false,
+                              isEmail: true,
+                              textController: emailController,
+                            ),
+                            Input(
+                              icon: Icons.lock_outline_sharp,
+                              hintText: 'Password',
+                              isPassword: true,
+                              isEmail: false,
+                              textController: passwordController,
+                            ),
+                            ElevatedButton.icon(
+                              onPressed: () {
+                                loginWithUsernamePassword();
+                              },
+                              style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                backgroundColor: UI.appColor,
+                              ),
+                              icon: Icon(Icons.login_sharp),
+                              label: Text("Login"),
                             )
+                          ]),
+                      Container(
+                        padding: EdgeInsets.only(top: 30),
+                        width: 300,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/signup');
+                              },
+                              child: Text(
+                                'Register',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/forgot');
+                              },
+                              child: Text(
+                                'Forgot password?',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                    ),
-                  ))
-            ],
-          ),
-        )
-      ],
-    ));
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   void loginWithUsernamePassword() {
@@ -159,6 +164,7 @@ class _LoginState extends State<Login> {
     FirebaseAuth.instance
         .signInWithEmailAndPassword(email: email, password: password)
         .then((value) {
+      print(value);
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => Homepage()),
