ca3c077ef7f770707caf4370e9aac20fe5525f5e
timothy tran <tadeothien@gmail.com>
2022-05-14 04:59:32 +0700

    nav bar fix

--- lib/utils/app_ui.dart
+++ lib/utils/app_ui.dart
@@ -6,19 +6,29 @@ class UI {
   static const String imagespath = 'assets/images/';
 
   static const String appLogo = "assets/images/appLogo.png";
-  static const String home = "assets/icons/homeActive.png";
-  static const String notify = "assets/icons/notify.png";
-  static const String bookmark = "assets/icons/bookmark.png";
-  static const String create = "assets/icons/create.png";
+
   static const String lib = "assets/icons/lib.png";
   static const String search = "assets/icons/search.png";
-  static const String account = "assets/icons/account.png";
+
   static const String setting = "assets/icons/setting.png";
   static const String arrowRight = "assets/icons/arrowright.png";
   static const Color appColor = Color(0xFF51B418);
   static const Color selectColor = Color.fromARGB(255, 46, 104, 13);
   static const TextStyle h1 =
       TextStyle(color: UI.appColor, fontSize: 50, fontWeight: FontWeight.w500);
+
+  //navbar
+  static const String homeActive = "assets/icons/homeActive.png";
+  static const String bookmarkActive = "assets/icons/bookmarkActive.png";
+  static const String createActive = "assets/icons/createActive.png";
+  static const String notifyActive = "assets/icons/notificationActive.png";
+  static const String accountActive = "assets/icons/profileActive.png";
+
+  static const String home = "assets/icons/homeInactive.png";
+  static const String bookmark = "assets/icons/bookmarkInactive.png";
+  static const String create = "assets/icons/createInactive.png";
+  static const String notify = "assets/icons/notificationInactive.png";
+  static const String account = "assets/icons/profileInactive.png";
 
   static const double topPadding = 50;
 }
