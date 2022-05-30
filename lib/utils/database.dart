0c93375a4188366557451bf5105e72e1860f6441
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-05-30 22:48:55 +0700

    update

--- lib/utils/database.dart
+++ lib/utils/database.dart
@@ -18,7 +18,7 @@ const List category = [
   "cat10"
 ];
 
-const List song_type_1 = [
+const List recipeCategories = [
   "Cơm",
   "Gỏi",
   "Bún",
@@ -27,15 +27,8 @@ const List song_type_1 = [
   "Bánh",
   "Đồ chay"
 ];
-const List song_type_2 = [
-  "Podcasts",
-  "Made For You",
-  "Charts",
-  "New Releases",
-  "Discover",
-  "Concerts",
-];
-const List songs = [
+
+const List mockRecipes = [
   {
     "img": "assets/images/MIT2021.png",
     "title": "Bánh cuốn",
@@ -211,199 +204,6 @@ const List songs = [
     "title": "Bánh cuốn",
     "description":
         "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-    "song_count": "75 songs",
-    "date": "3 hr 56 min",
-    "color": Color(0xFF4e6171),
-    "song_url": "songs/2.mp3",
-    "songs": [
-      {"title": "Imagination", "duration": "1:21"},
-      {"title": "Home_", "duration": "2:17"},
-      {"title": "Do I Wanna Know?", "duration": "1:31"},
-      {"title": "Whiskey Sour", "duration": "1:42"},
-      {"title": "Decisions", "duration": "1:29"},
-      {"title": "Trees", "duration": "1:51"},
-      {"title": "Earth", "duration": "1:39"},
-    ]
-  }
-];
-
-const List drinks = [
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Trà dâu",
-    "description":
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-    "song_count": "100 songs",
-    "date": "about 19 hr",
-    "color": Color(0xFFf69129),
-    "song_url": "songs/1.mp3",
-    "songs": [
-      {"title": "Imagination", "duration": "1:21"},
-      {"title": "Home_", "duration": "2:17"},
-      {"title": "Do I Wanna Know?", "duration": "1:31"},
-      {"title": "Whiskey Sour", "duration": "1:42"},
-      {"title": "Decisions", "duration": "1:29"},
-      {"title": "Trees", "duration": "1:51"},
-      {"title": "Earth", "duration": "1:39"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Trà sữa",
-    "description":
-        "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-    "song_count": "324 songs",
-    "date": "about 14 hr",
-    "color": Color(0xFF64849c),
-    "song_url": "songs/2.mp3",
-    "songs": [
-      {"title": "Kaleidoscope", "duration": "2:01"},
-      {"title": "Larks", "duration": "2:54"},
-      {"title": "Homeland", "duration": "2:22"},
-      {"title": "Une Danse", "duration": "3:03"},
-      {"title": "Almonte", "duration": "2:31"},
-      {"title": "Days Like These", "duration": "4:09"},
-      {"title": "In questo momento", "duration": "2:40"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Trà đào",
-    "description":
-        "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-    "song_count": "195 songs",
-    "date": "about 10 hr",
-    "color": Color(0xFF58546c),
-    "song_url": "songs/1.mp3",
-    "songs": [
-      {"title": "Escaping Time", "duration": "3:20"},
-      {"title": "Just Look at You", "duration": "3:07"},
-      {"title": "Flowing", "duration": "2:11"},
-      {"title": "With Resolve", "duration": "2:09"},
-      {"title": "Infinite Sustain", "duration": "2:29"},
-      {"title": "Ingénue", "duration": "2:38"},
-      {"title": "Hidden Chambers", "duration": "2:49"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Cà phê",
-    "description":
-        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    "song_count": "599 songs",
-    "date": "about 21 hr",
-    "color": Color(0xFFbad6ec),
-    "song_url": "songs/2.mp3",
-    "songs": [
-      {"title": "Imagination", "duration": "1:21"},
-      {"title": "Home_", "duration": "2:17"},
-      {"title": "Do I Wanna Know?", "duration": "1:31"},
-      {"title": "Whiskey Sour", "duration": "1:42"},
-      {"title": "Decisions", "duration": "1:29"},
-      {"title": "Trees", "duration": "1:51"},
-      {"title": "Earth", "duration": "1:39"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Sinh tố bơ",
-    "description":
-        "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-    "song_count": "317 songs",
-    "date": "about 11 hr",
-    "color": Color(0xFF93689a),
-    "song_url": "songs/1.mp3",
-    "songs": [
-      {"title": "Imagination", "duration": "1:21"},
-      {"title": "Home_", "duration": "2:17"},
-      {"title": "Do I Wanna Know?", "duration": "1:31"},
-      {"title": "Whiskey Sour", "duration": "1:42"},
-      {"title": "Decisions", "duration": "1:29"},
-      {"title": "Trees", "duration": "1:51"},
-      {"title": "Earth", "duration": "1:39"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Trà sữa khổng lồ",
-    "description":
-        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
-    "song_count": "130 songs",
-    "date": "about 7 hr",
-    "color": Color(0xFFa4c4d3),
-    "song_url": "songs/2.mp3",
-    "songs": [
-      {"title": "Imagination", "duration": "1:21"},
-      {"title": "Home_", "duration": "2:17"},
-      {"title": "Do I Wanna Know?", "duration": "1:31"},
-      {"title": "Whiskey Sour", "duration": "1:42"},
-      {"title": "Decisions", "duration": "1:29"},
-      {"title": "Trees", "duration": "1:51"},
-      {"title": "Earth", "duration": "1:39"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Chè Thái",
-    "description":
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "song_count": "50 songs",
-    "date": "about 17 hr",
-    "color": Color(0xFF5e4f78),
-    "song_url": "songs/1.mp3",
-    "songs": [
-      {"title": "Kaleidoscope", "duration": "2:01"},
-      {"title": "Larks", "duration": "2:54"},
-      {"title": "Homeland", "duration": "2:22"},
-      {"title": "Une Danse", "duration": "3:03"},
-      {"title": "Almonte", "duration": "2:31"},
-      {"title": "Days Like These", "duration": "4:09"},
-      {"title": "In questo momento", "duration": "2:40"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Chè bưởi",
-    "description":
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "song_count": "69 songs",
-    "date": "2 hr 14 min",
-    "color": Color(0xFFa4c1ad),
-    "song_url": "songs/2.mp3",
-    "songs": [
-      {"title": "Escaping Time", "duration": "3:20"},
-      {"title": "Just Look at You", "duration": "3:07"},
-      {"title": "Flowing", "duration": "2:11"},
-      {"title": "With Resolve", "duration": "2:09"},
-      {"title": "Infinite Sustain", "duration": "2:29"},
-      {"title": "Ingénue", "duration": "2:38"},
-      {"title": "Hidden Chambers", "duration": "2:49"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Chè đậu đen",
-    "description":
-        "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-    "song_count": "60 songs",
-    "date": "3 hr 25 min",
-    "color": Color(0xFFd9e3ec),
-    "song_url": "songs/1.mp3",
-    "songs": [
-      {"title": "Imagination", "duration": "1:21"},
-      {"title": "Home_", "duration": "2:17"},
-      {"title": "Do I Wanna Know?", "duration": "1:31"},
-      {"title": "Whiskey Sour", "duration": "1:42"},
-      {"title": "Decisions", "duration": "1:29"},
-      {"title": "Trees", "duration": "1:51"},
-      {"title": "Earth", "duration": "1:39"},
-    ]
-  },
-  {
-    "img": "assets/images/MIT2021.png",
-    "title": "Chè đậu xanh",
-    "description":
-        "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
     "song_count": "75 songs",
     "date": "3 hr 56 min",
     "color": Color(0xFF4e6171),
